0968034ab56cefd6e81c968bc3e59742868ba1ff
Juan Pablo Pérez <juan.perez@lorspi.com>
2022-07-25 17:38:57 -0500

    2507.4
    Chat renovado.

--- behavior_packs/Comerciantes Applelandia Behavior/scripts/index.js
+++ behavior_packs/Comerciantes Applelandia Behavior/scripts/index.js
@@ -1,2 +1,18 @@
 import 'scripts/items.js'
-import 'scripts/chat.js'
+import 'scripts/comandos.js'
+
+import { chatrank } from 'scripts/chat.js'
+import { world } from 'mojang-minecraft'
+let tick = 0, worldLoaded = false, loadTime = 0;
+
+world.events.beforeChat.subscribe((data) => {
+    chatrank(data)
+})
+world.events.tick.subscribe((ticks) => {
+    tick++
+    if (!world.getDimension("overworld").runCommand('testfor @a').error && !worldLoaded) {
+        loadTime = tick
+        worldLoaded = true;
+        world.getDimension("overworld").runCommand(`tellraw @a {"rawtext":[{"text":"World has loaded in ${loadTime} ticks!"}]}`)
+    }
+})
