6aa5adf6b0b22abed7491b7767a7edf7424cb659
Juan Pablo Pérez <juan.perez@lorspi.com>
2022-07-28 11:22:49 -0500

    2807.3
    Agregados encantamientos a la interfaz de encantamientos.

--- behavior_packs/Comerciantes Applelandia Behavior/scripts/entidades.js
+++ behavior_packs/Comerciantes Applelandia Behavior/scripts/entidades.js
@@ -22,7 +22,16 @@ function encantamientos(player) {
     let form = new ActionFormData()
         form.title("§l§5Encantamientos") // Title of the GUI
         form.body("Elige el encantamiento para aplicar a tu objeto en mano.\n\n") 
-        form.button("§l§5Irrompible III\n§r§820 niveles") 
+        form.button("§l§5Irrompible III\n§r§820 niveles")
+        form.button("§l§5Agilidad acuática III\n§r§820 niveles")
+        form.button("§l§5Afinidad acuática\n§r§820 niveles")
+        form.button("§l§5Respiración III\n§r§820 niveles")
+        form.button("§l§5Saqueo 3\n§r§830 niveles")
+        form.button("§l§5Toque de seda\n§r§820 niveles")
+        form.button("§l§5Lealtad III\n§r§820 niveles")
+        form.button("§l§5Protección IV\n§r§820 niveles")
+        form.button("§l§5Fortuna III\n§r§830 niveles")
+        form.button("§l§5Reparación\n§r§860 niveles")
         form.button("§4Cerrar") 
 
         form.show(player).then((response) => {
@@ -32,6 +41,105 @@ function encantamientos(player) {
                     try{
                         player.runCommand('enchant @s unbreaking 3')
                         player.runCommand('xp -20l @s')
+                        player.runCommand('playsound random.orb @s')
+                        encantado(player)
+                    } catch {nocompatible(player)}                
+                }catch{noniveles(player)}
+            }
+            if (response.selection === 1) {
+                try{
+                    player.runCommand('testfor @s[lm=20]')
+                    try{
+                        player.runCommand('enchant @s depth_strider 3')
+                        player.runCommand('xp -20l @s')
+                        player.runCommand('playsound random.orb @s')
+                        encantado(player)
+                    } catch {nocompatible(player)}                
+                }catch{noniveles(player)}
+            }
+            if (response.selection === 2) {
+                try{
+                    player.runCommand('testfor @s[lm=20]')
+                    try{
+                        player.runCommand('enchant @s aqua_affinity')
+                        player.runCommand('xp -20l @s')
+                        player.runCommand('playsound random.orb @s')
+                        encantado(player)
+                    } catch {nocompatible(player)}                
+                }catch{noniveles(player)}
+            }
+            if (response.selection === 3) {
+                try{
+                    player.runCommand('testfor @s[lm=20]')
+                    try{
+                        player.runCommand('enchant @s respiration 3')
+                        player.runCommand('xp -20l @s')
+                        player.runCommand('playsound random.orb @s')
+                        encantado(player)
+                    } catch {nocompatible(player)}                
+                }catch{noniveles(player)}
+            }
+            if (response.selection === 4) {
+                try{
+                    player.runCommand('testfor @s[lm=30]')
+                    try{
+                        player.runCommand('enchant @s looting 3')
+                        player.runCommand('xp -30l @s')
+                        player.runCommand('playsound random.orb @s')
+                        encantado(player)
+                    } catch {nocompatible(player)}                
+                }catch{noniveles(player)}
+            }
+            if (response.selection === 5) {
+                try{
+                    player.runCommand('testfor @s[lm=20]')
+                    try{
+                        player.runCommand('enchant @s silk_touch')
+                        player.runCommand('xp -20l @s')
+                        player.runCommand('playsound random.orb @s')
+                        encantado(player)
+                    } catch {nocompatible(player)}                
+                }catch{noniveles(player)}
+            }
+            if (response.selection === 6) {
+                try{
+                    player.runCommand('testfor @s[lm=20]')
+                    try{
+                        player.runCommand('enchant @s loyalty 3')
+                        player.runCommand('xp -20l @s')
+                        player.runCommand('playsound random.orb @s')
+                        encantado(player)
+                    } catch {nocompatible(player)}                
+                }catch{noniveles(player)}
+            }
+            if (response.selection === 7) {
+                try{
+                    player.runCommand('testfor @s[lm=20]')
+                    try{
+                        player.runCommand('enchant @s protection 4')
+                        player.runCommand('xp -20l @s')
+                        player.runCommand('playsound random.orb @s')
+                        encantado(player)
+                    } catch {nocompatible(player)}                
+                }catch{noniveles(player)}
+            }
+            if (response.selection === 8) {
+                try{
+                    player.runCommand('testfor @s[lm=30]')
+                    try{
+                        player.runCommand('enchant @s fortune 3')
+                        player.runCommand('xp -30l @s')
+                        player.runCommand('playsound random.orb @s')
+                        encantado(player)
+                    } catch {nocompatible(player)}                
+                }catch{noniveles(player)}
+            }
+            if (response.selection === 9) {
+                try{
+                    player.runCommand('testfor @s[lm=60]')
+                    try{
+                        player.runCommand('enchant @s mending')
+                        player.runCommand('xp -60l @s')
                         player.runCommand('playsound random.orb @s')
                         encantado(player)
                     } catch {nocompatible(player)}                
