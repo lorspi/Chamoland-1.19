01c7fc29ece25280c307c047a339dae8ff76983a
Juan Pablo Pérez <juan.perez@lorspi.com>
2022-08-02 12:14:50 -0500

    208.2
    Ajuste del replicador de criaturas para que sea válido con aldeanos v2.

--- behavior_packs/Comerciantes Applelandia Behavior/scripts/captura.js
+++ behavior_packs/Comerciantes Applelandia Behavior/scripts/captura.js
@@ -10,7 +10,7 @@ const overworld = world.getDimension("overworld")
 var validarherramienta = false
 var validarmob = false
 var herramienta = "npc:talismancap"
-const mobs = ["cow", "sheep", "chicken" , "pig", "skeleton", "zombie", "wither_skeleton", "creeper", "zombie_villager", "villager", "husk", "skeleton_horse", "donkey", "mule", "spider", "slime", "ghast", "zombie_pigman", "enderman", "cave_spider", "blaze", "magma_cube", "bat", "endermite", "guardian", "shulker", "squid", "wolf", "mooshroom", "ocelot", "horse", "rabbit", "polar_bear", "llama", "parrot", "goat", "phantom", "fox", "turtle", "frog", "dolphin"];
+const mobs = ["cow", "sheep", "chicken" , "pig", "skeleton", "zombie", "wither_skeleton", "creeper", "zombie_villager", "villager", "villager_v2", "husk", "skeleton_horse", "donkey", "mule", "spider", "slime", "ghast", "zombie_pigman", "enderman", "cave_spider", "blaze", "magma_cube", "bat", "endermite", "guardian", "shulker", "squid", "wolf", "mooshroom", "ocelot", "horse", "rabbit", "polar_bear", "llama", "parrot", "goat", "phantom", "fox", "turtle", "frog", "dolphin", "cat"];
 
 world.events.entityHit.subscribe(event => {
 
@@ -33,10 +33,18 @@ world.events.entityHit.subscribe(event => {
             validarmob = false
         }
 
+        //overworld.runCommand(`tellraw @a {"rawtext":[{"text":"ID: ${event.hitEntity.id.substring(10)}"}]}`)
+        //overworld.runCommand(`tellraw @a {"rawtext":[{"text":"Herra: ${validarherramienta} Mob: ${validarmob}"}]}`)
+
         if(validarherramienta && validarmob){
 
             player.runCommand(`clear @s ${herramienta} 0 1`)
-            player.runCommand(`give ${event.entity.name} ${event.hitEntity.id.substring(10)}_spawn_egg`)
+            
+            if(event.hitEntity.id.substring(10) == "villager_v2"){
+                player.runCommand(`give ${event.entity.name} villager_spawn_egg`)
+            } else {
+                player.runCommand(`give ${event.entity.name} ${event.hitEntity.id.substring(10)}_spawn_egg`)
+            }
 
             player.runCommand(`playsound random.totem @s ~ ~ ~ 0.5`)
             player.runCommand(`particle minecraft:totem_particle ~ ~ ~`)
