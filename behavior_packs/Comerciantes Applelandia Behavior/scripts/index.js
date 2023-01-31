6449df4b5aa3c5b28aed5c79e2edf14c62ee6f88
Juan Pablo Pérez <juan.perez@lorspi.com>
2023-01-31 16:31:02 -0500

    Actualización y pruebas
    - Actualización 1.19.50.
    - Pruebas para arreglar los scripts fallidas.

--- behavior_packs/Comerciantes Applelandia Behavior/scripts/index.js
+++ behavior_packs/Comerciantes Applelandia Behavior/scripts/index.js
@@ -1,10 +1,10 @@
-import "items.js"
-import "comandos.js"
-import "entidades.js"
-import "ticks.js"
-import "dormir.js"
-import "captura.js"
-import "particulas.js"
+//import "items.js"
+//import "comandos.js"
+//import "entidades.js"
+//import "ticks.js"
+//import "dormir.js"
+//import "captura.js"
+//import "particulas.js"
 //import "luz.js"
 
 import { chatrank } from "chat.js"
@@ -12,6 +12,7 @@ import { world } from "@minecraft/server"
 let tick = 0, worldLoaded = false, loadTime = 0;
 
 world.events.beforeChat.subscribe((data) => {
+    overworld.runCommand('playsound random.orb @a') //Prueba si entra en el evento
     chatrank(data)
 })
 world.events.tick.subscribe((ticks) => {
@@ -24,10 +25,7 @@ world.events.tick.subscribe((ticks) => {
 })
 
 //Constant tick
-World.events.tick.subscribe(() => {
- 
+world.events.tick.subscribe(() => {
     // Reproducir sonido una sola vez al pasar sobre un bloque específico.
-    overworld.runCommand('execute @a ~ ~ ~ detect ~ ~-2 ~ npc:adalita_block 0 playsound random.orb @p')
-    
+    overworld.runCommand('execute @a ~ ~ ~ detect ~ ~-2 ~ npc:adalita_block 0 playsound random.orb @p')    
 })
-
